ac02ac6345691bbfd8029f0d1eb33d102079846b
birdMan <314269883@qq.com>
2025-06-13 11:47:16 +0800

    Update build-libbox.sh

--- build-libbox.sh
+++ build-libbox.sh
@@ -15,12 +15,7 @@ echo "🔧 初始化 go.mod & 依赖..."
 go mod tidy
 go get golang.org/x/mobile/bind
 
-echo "⚙️ 使用 gomobile 构建各平台版本（生成 .framework 文件夹）..."
-gomobile bind -tags with_utls -target=ios/arm64 -o libbox_ios_arm64 .
-gomobile bind -tags with_utls -target=iossimulator/arm64 -o libbox_iossim_arm64 .
-gomobile bind -tags with_utls -target=macos/arm64 -o libbox_macos_arm64 .
-
-echo "✅ 编译完成，输出目录如下："
-echo " - libbox_ios_arm64.framework"
-echo " - libbox_iossim_arm64.framework"
-echo " - libbox_macos_arm64.framework"
+echo "⚙️ 使用 gomobile 构建各平台版本..."
+gomobile bind -tags with_utls -target=ios/arm64 -o libbox_ios_arm64.xcframework .
+gomobile bind -tags with_utls -target=iossimulator/arm64 -o libbox_iossim_arm64.xcframework .
+gomobile bind -tags with_utls -target=macos/arm64 -o libbox_macos_arm64.xcframework .
